1aa0d357018134b138a2f3ba888ad2e313884607
Manuz6 <senno.emmanuel92@gmail.com>
2024-07-03 10:45:03 +0100

    Update ejemplo1.py

--- ejemplo1.py
+++ ejemplo1.py
@@ -10,7 +10,7 @@ mensaje2()
 def sumar(n1,n2):
     print('El resultado de la suma entre n1 + n2 es de: ', n1 + n2)
 
-sumar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el primer numero: ")))
+sumar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
 
 def multiplicar(n1,n2):
     print('El resultado de la multiplicacion entre n1 * n2 es de: ', n1 * n2)
